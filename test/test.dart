cbede19656ad254d67090625be3e0caa21b5ed34
itsmohsinzaidi <itsmohsinzaidi@users.noreply.github.com>
2020-08-05 17:13:18 +0500

    testing

---
+++ test/test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(TestWidget());
+}
+
+class TestWidget extends StatefulWidget {
+  @override
+  _TestWidgetState createState() => _TestWidgetState();
+}
+
+class _TestWidgetState extends State<TestWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
